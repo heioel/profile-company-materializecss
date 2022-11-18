304deb74ef8575b78cc453dc5cd010332f2fa9a2
heioel <tukilmuah@gmail.com>
2022-11-19 06:33:49 +0700

    feature slider

--- js/script.js
+++ js/script.js
@@ -1,2 +1,9 @@
 const sideNav = document.querySelectorAll('.sidenav');
 M.Sidenav.init(sideNav);
+
+const slider = document.querySelectorAll('.slider');
+M.Slider.init(slider,{
+    height : 590,
+    duration : 300,
+    interval : 2000
+});
