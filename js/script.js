958d0b762e00c63563968968c61a7d561daf9b15
heioel <tukilmuah@gmail.com>
2022-11-30 10:40:18 +0700

    add image on feature media and client

--- js/script.js
+++ js/script.js
@@ -2,10 +2,10 @@ const sideNav = document.querySelectorAll('.sidenav');
 M.Sidenav.init(sideNav);
 
 const slider = document.querySelectorAll('.slider');
-M.Slider.init(slider,{
-    height : 570,
-    duration : 300,
-    interval : 2000
+M.Slider.init(slider, {
+    height: 570,
+    duration: 300,
+    interval: 2000
 });
 
 const materialBox = document.querySelectorAll('.materialboxed');
@@ -13,3 +13,8 @@ M.Materialbox.init(materialBox);
 
 const collap = document.querySelectorAll('.collapsible');
 M.Collapsible.init(collap);
+
+const scroll = document.querySelectorAll('.scrollspy');
+M.ScrollSpy.init(scroll,{
+    scrollOffset :50
+});
